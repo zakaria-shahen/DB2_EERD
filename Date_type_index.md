fb705141e3d350a2f9987c6c2e22fc8e65f310cd
Zakaria-Shahen <zikoshahen1@gmial.com>
2021-12-18 04:23:29 +0200

    add sql schema and 70% review

---
+++ Date_type_index.md
@@ -0,0 +1,15 @@
+## Data Type 
+
+- IDs => smallint
+- IDs(phone table,..etc ) => tinyint
+- bool => bit
+- string medium (english only) Ex. Full-Name, Email  => varchar(40) 
+- string larg (english only)  =>  text
+- string small (english only) Ex. Phone, first/last-name => varchar(20) 
+- salary, price => smallmoney
+- 
+
+zakaria shahne saleh
+zakariaShahen
+
+## using index 
--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -1,110 +1,250 @@
-CREATE DATABASE Db2_Project;
-USE Db2_Project;
---------------------------------------------------------
+-- CREATE DATABASE Db2_2;
+-- USE Db2_2;
+-- --------------------------------------------------------
 
-drop table if exists Room_Occupancy
-create table Room_Occupancy
-(
-    id tinyint primary key,
-    name_ char(10)
-)
-
-drop table if exists Room_Bed
-create table Room_Bed
-(
-    id tinyint primary key,
-    name_ char(10)
-)
-
-drop table if exists Room_Layout
-create table Room_Layout
-(
-    id tinyint primary key,
-    name_ char(10)
-)
-
---------------------------------------------------------
-
-drop table if exists Room_Category
-create table Room_Category
-(
-    id tinyint primary key,
+create table Room_Occupancy(
+    id tinyint,
     name_ char(10),
-    room_occupancy tinyint foreign key references Room_Occupancy(id),
-    room_bed tinyint foreign key references Room_Bed(id),
-    room_layout tinyint foreign key references Room_Layout(id)
+    primary key(id)
 )
 
-drop table if exists Rooms
-create table Rooms
-(
-    id smallint primary key,
-    room_category tinyint foreign key references Room_Category(id)
+create table Room_Bed(
+    id tinyint,
+    name_ char(10),
+    primary key(id)
+)
+
+create table Room_Layout(
+    id tinyint,
+    name_ char(10),
+    primary key(id)
+)
+
+--------------------------------------------------------
+
+create table Room_Category(
+    id tinyint,
+    name_ char(10),
+    room_occupancy tinyint,
+    room_bed tinyint,
+    room_layout tinyint,
+    primary key(id),
+    foreign key(room_occupancy) references Room_Occupancy(id),
+    foreign key(room_bed) references Room_Bed(id),
+    foreign key(room_layout) references Room_Layout(id)
+)
+
+create table Rooms(
+    id smallint,
+    room_category tinyint,
+    primary key(id),
+    foreign key(room_category) references Room_Category(id)
 )
 
 ----------------------------------------
 
-drop table if exists Booked_Rooms
-create table Booked_Rooms
-(
-    id smallint primary key,
+create table Booked_Rooms(
+    id smallint,
     booking_date datetime2,
     check_in_date datetime2,
     check_out_date datetime2,
-    duration as check_out_date - check_in_date,
-    total_price smallmoney
+    -- duration as check_out_date - check_in_date, -- invalid syntaxt => move to select (only)
+    -- total_price smallmoney, -- why?Edit here Comment: @zakaria-shshen
+    -- Where Loaction attr?
+    primary key(id)
 )
 
-drop table if exists Used_Rooms
-create table Used_Rooms
-(
-    rooms smallint foreign key references Rooms(id),
-    booked_rooms smallint foreign key references Booked_Rooms(id),
-    primary key (rooms, booked_rooms)
+
+create table Booked_Rooms__Rooms(
+    rooms smallint,
+    booked_rooms smallint,
+    primary key (rooms, booked_rooms),
+    foreign key(booked_rooms) references Booked_Rooms(id),
+    foreign key(rooms) references Rooms(id)
 )
 
 -------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------
 
-drop table if exists Services
-create table Services
-(
-    id tinyint primary key,
+create table Services(
+    id tinyint,
     name_ varchar(20),
     details text,
-    price smallmoney
+    price smallmoney,
+    primary key(id)
 )
 
-drop table if exists Booked_Services
-create table Booked_Services
-(
-    id smallint primary key,
+-- this Table(Booked_Services) remove (in review)
+create table Booked_Services(
+    id smallint,
     booking_date datetime2,
-    total_price smallmoney
+    total_price smallmoney, -- Look here
+    services tinyint,
+    primary key(id, services),
+    foreign key(services) references Services(id)
 )
 
-drop table if exists Used_Services
-create table Used_Services
-(
-    services tinyint foreign key references Services(id) ,
-    booked_services tinyint foreign key references Booked_Services(id) ,
-    primary key (services, booked_services)
+create table Booked_Services__Services(
+    services tinyint,
+    booked_services smallint,
+    primary key (services, booked_services),
+    foreign key(booked_services) references Booked_Services(id),
+    foreign key(services) references Services(id)
 )
 
 ---------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------
 
-drop table if exists Companion
-create table Companion
-(
-    id smallint primary key,
+create table Companion(
+    id smallint,
     full_name varchar(40),
+    primary key(id)
 )
 
-drop table if exists Bookings_Details__Companion
-create table Bookings_Details__Companion
-(
-    companion smallint foreign key references Companion(id),
-    bookings_details smallint foreign key references Bookings_Details(id),
-    primary key (companion, bookings_details)
+create table Bookings_Details__Companion(
+    companion smallint,
+    bookings_details smallint,
+    primary key (companion, bookings_details),
+    foreign key(companion) references Companion(id),
+    foreign key(bookings_details) references Bookings_Details(id),
 )
+
+
+
+
+
+-- 
+-- @zakaria-shahen next..
+-- 
+
+-- create database DB2_Project;
+-- use DB2_Project;
+
+create table People(
+    id smallint,
+    f_name varchar(20),
+    m_name varchar(20),
+    l_name varchar(20),
+    country varchar(20),
+    city varchar(20),
+    street varchar(40),
+    nationality_code char(5),
+    brith date,
+    email varchar(40),
+    primary key(id)
+);
+
+create table People_Phone(
+    id smallint,
+    phone varchar(20),
+    primary key(id, phone),
+    foreign key(id) references People(id)
+);
+
+create index phone_index on People_Phone(phone);
+
+create table Guests(
+    id smallint,
+    primary key(id),
+    foreign key(id) references People(id)
+);
+
+
+create table Relationship(
+    id tinyint identity(1, 1),
+    type_ varchar(20),
+    primary key(id)
+)
+
+
+create table Family(
+    id tinyint,
+    name_ varchar(40),
+    guest smallint,
+    relationship tinyint,
+    primary key(id, guest), 
+    foreign key(guest) references Guests(id),
+    foreign key(relationship) references Relationship(id)
+);
+
+create index guest on Family(guest);
+create index relationship on Family(relationship);
+
+create table Payment_method(
+    id tinyint,
+    type_ varchar(10),
+    primary key(id)
+); 
+
+-- Payment method
+create table Bookings_Details(
+    id tinyint identity(1, 1), -- Look here
+    card_number tinyint,
+    favorite bit,
+    guests smallint unique not null,
+    payment_method tinyint unique not null,
+    primary key (id),
+    foreign key(guests) references Guests(id),
+    foreign key(payment_method) references Payment_method(id) 
+);
+
+create index guests on Bookings_Details(guests);
+create index payment_method on Bookings_Details(payment_method);
+
+create table Invoice(
+    id smallint,
+    fees smallmoney,
+    price smallmoney,
+    date_ date,
+    bookings_details tinyint unique not null,
+    primary key (id),
+    foreign key(bookings_details) references Bookings_Details
+);
+
+create index bookings_details on Invoice(bookings_details)
+
+create table Discount(
+    id tinyint,
+    type_ varchar(40),
+    discount_rate tinyint,
+    start_ datetime2, 
+    end_ datetime2,
+    primary key(id)
+);
+
+create table Discount_Invoice(
+    invoice smallint,
+    discount tinyint,
+    primary key(invoice, discount),
+    foreign key(invoice) references Invoice(id),
+    foreign key(discount) references Discount(id)
+
+);
+
+create index invoice on Discount_Invoice(invoice);
+create index discount on Discount_Invoice(discount);
+
+create table Job_Title(
+    id tinyint,
+    name_ varchar(40),
+    primary key(id)
+);
+
+
+create table Employees(
+    id smallint,
+    salary smallmoney,
+    work_hours tinyint,
+    hire_date date,
+    creer_level tinyint,
+    job_title tinyint,
+    primary key(id),
+    foreign key(id) references People(id),
+    foreign key(job_title) references Job_Title(id)
+);
+
+create index job_title on Employees(job_title);
+
+-- Get a list of tables and views in the current database
+SELECT table_catalog [database], table_schema [schema], table_name [name], table_type [type]
+FROM INFORMATION_SCHEMA.TABLES;
